75ce29ba458e2063b8266e83b8ce8400470f5aed
veselingifted <136447285+veselingifted@users.noreply.github.com>
2024-09-11 19:41:24 +0300

    style: Hero text animation and css finished, show my work buton/css added

--- portfolio/app/layout.tsx
+++ portfolio/app/layout.tsx
@@ -1,17 +1,13 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "./provider";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+const inter = Inter({
+  subsets: ["latin"], // Choose the subset you need, e.g., latin
+  variable: "--font-inter", // Set a CSS variable for the font
+  display: "swap", // Optional, controls font loading behavior
 });
 
 export const metadata: Metadata = {
@@ -26,16 +22,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body className={inter.variable}>
         <ThemeProvider
-            attribute="class"
-            defaultTheme="dark"
-            enableSystem
-            disableTransitionOnChange
-          >
-            {children}
-          </ThemeProvider>
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
